ec5e61c5079b1e4301a4f070a625f9613fb65efb
jimfleming <jimmy.fleming@gmail.com>
2014-04-25 22:44:17 -0700

    Adding string and array extensions and logger

---
+++ HasteLogger.cs
@@ -0,0 +1,31 @@
+using UnityEngine;
+using System;
+using System.Linq;
+using System.Collections;
+
+public static class Logger {
+
+  public static void Info(params object[] args) {
+    string[] sargs = ((IEnumerable)args)
+      .Cast<object>()
+      .Select(x => x.ToString())
+      .ToArray();
+    Debug.Log(String.Join(", ", sargs));
+  }
+
+  public static void Warn(params object[] args) {
+    string[] sargs = ((IEnumerable)args)
+      .Cast<object>()
+      .Select(x => x.ToString())
+      .ToArray();
+    Debug.LogWarning(String.Join(", ", sargs));
+  }
+
+  public static void Error(params object[] args) {
+    string[] sargs = ((IEnumerable)args)
+      .Cast<object>()
+      .Select(x => x.ToString())
+      .ToArray();
+    Debug.LogError(String.Join(", ", sargs));
+  }
+}
