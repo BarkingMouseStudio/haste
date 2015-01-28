a73a8d000a55699cea68f33162515a701d2b88af
jimfleming <jimmy.fleming@gmail.com>
2015-01-27 17:04:02 -0800

    Adding resource manager

---
+++ Assets/Haste/Editor/HasteResources.cs
@@ -0,0 +1,46 @@
+using System;
+using System.IO;
+using UnityEngine;
+using UnityEditor;
+
+namespace Haste {
+
+  public static class HasteResources {
+
+    private static readonly string partialPath = "/Haste/Editor/InternalResources/";
+
+    private static string internalResourcesPath = "";
+    public static string InternalResourcesPath {
+      get {
+        if (string.IsNullOrEmpty(internalResourcesPath)) {
+          string path;
+          if (FindInternalResourcesPath(out path)) {
+            internalResourcesPath = path;
+          } else {
+            Debug.LogError("Unable to locate the internal resources folder. Make sure your Haste installation is intact.");
+            HasteWindow.Instance.Close();
+          }
+        }
+        return internalResourcesPath;
+      }
+    }
+
+    public static T Load<T>(string path) where T : UnityEngine.Object {
+      return (T)AssetDatabase.LoadAssetAtPath(InternalResourcesPath + path, typeof(T));
+    }
+
+    private static bool FindInternalResourcesPath(out string path) {
+      foreach (string assetPath in AssetDatabase.GetAllAssetPaths()) {
+        var index = assetPath.IndexOf(partialPath);
+        if (index != -1) {
+          path = assetPath.Substring(0, index) + partialPath;
+          return true;
+        }
+      }
+
+      path = "";
+      return false;
+    }
+  }
+}
+
