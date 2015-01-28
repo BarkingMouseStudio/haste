ca63f58af2b860911f38ec8decfe2b1def108dc9
jimfleming <jimmy.fleming@gmail.com>
2015-01-27 17:03:45 -0800

    Renaming HasteLogger and adding more overloads

--- Assets/Haste/Editor/Utils/HasteDebug.cs
+++ Assets/Haste/Editor/Utils/HasteDebug.cs
@@ -1,5 +1,3 @@
-#define DEBUG
-
 using UnityEngine;
 using System;
 using System.Linq;
@@ -7,7 +5,7 @@ using System.Collections;
 
 namespace Haste {
 
-  public static class HasteLogger {
+  public static class HasteDebug {
 
     [System.Diagnostics.Conditional("DEBUG")]
     public static void Info(params object[] args) {
@@ -16,6 +14,11 @@ namespace Haste {
         .Select(x => x == null ? "null" : x.ToString())
         .ToArray();
       Debug.Log(String.Join(", ", sargs));
+    }
+
+    [System.Diagnostics.Conditional("DEBUG")]
+    public static void Info(string format, params object[] args) {
+      Debug.Log(String.Format(format, args));
     }
 
     [System.Diagnostics.Conditional("DEBUG")]
@@ -28,12 +31,20 @@ namespace Haste {
     }
 
     [System.Diagnostics.Conditional("DEBUG")]
+    public static void Warn(string format, params object[] args) {
+      Debug.LogWarning(String.Format(format, args));
+    }
+
     public static void Error(params object[] args) {
       string[] sargs = ((IEnumerable)args)
         .Cast<object>()
         .Select(x => x == null ? "null" : x.ToString())
         .ToArray();
       Debug.LogError(String.Join(", ", sargs));
+    }
+
+    public static void Error(string format, params object[] args) {
+      Debug.LogError(String.Format(format, args));
     }
   }
 }
