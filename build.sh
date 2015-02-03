aafc445928655e0d5e2860c749931f53311c04f1
jimfleming <jimmy.fleming@gmail.com>
2015-02-03 14:13:56 -0800

    Logging in build script

--- build.sh
+++ build.sh
@@ -6,7 +6,7 @@ UNITYENGINE_PATH="${UNITY_PATH}/Contents/Frameworks/Managed/UnityEngine.dll"
 UNITYEDITOR_PATH="${UNITY_PATH}/Contents/Frameworks/Managed/UnityEditor.dll"
 
 PROJECT_PATH=$(pwd)
-SCRIPTS=$(find . -type f -name "*.cs")
+SCRIPTS=$(find ./Assets/Haste/Editor/ -type f -name "*.cs")
 
 HASTE_FREE_NAME="HasteFree.dll"
 HASTE_PRO_NAME="HastePro.dll"
@@ -16,3 +16,5 @@ mcs -r:$UNITYENGINE_PATH,$UNITYEDITOR_PATH -target:library -out:$HASTE_FREE_NAME
 
 echo "Building Haste Pro... ${PROJECT_PATH}/${HASTE_PRO_NAME}"
 mcs -r:$UNITYENGINE_PATH,$UNITYEDITOR_PATH -target:library -define:IS_HASTE_PRO -out:$HASTE_PRO_NAME $SCRIPTS
+
+echo "Done."
