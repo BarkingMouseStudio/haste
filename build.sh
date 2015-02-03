f2cfa8d53ac7af9cd89b7ae8958f0d7d98ab3417
jimfleming <jimmy.fleming@gmail.com>
2015-02-03 14:41:40 -0800

    Using gmcs in build script for Unity compat

--- build.sh
+++ build.sh
@@ -6,15 +6,15 @@ UNITYENGINE_PATH="${UNITY_PATH}/Contents/Frameworks/Managed/UnityEngine.dll"
 UNITYEDITOR_PATH="${UNITY_PATH}/Contents/Frameworks/Managed/UnityEditor.dll"
 
 PROJECT_PATH=$(pwd)
-SCRIPTS=$(find ./Assets/Haste/Editor/ -type f -name "*.cs")
+SCRIPTS=$(find ./Assets/Haste/Editor -type f -name "*.cs")
 
 HASTE_FREE_NAME="HasteFree.dll"
 HASTE_PRO_NAME="HastePro.dll"
 
 echo "Building Haste Free... ${PROJECT_PATH}/${HASTE_FREE_NAME}"
-mcs -r:$UNITYENGINE_PATH,$UNITYEDITOR_PATH -target:library -out:$HASTE_FREE_NAME $SCRIPTS
+gmcs -r:$UNITYENGINE_PATH,$UNITYEDITOR_PATH -target:library -out:$HASTE_FREE_NAME $SCRIPTS
 
 echo "Building Haste Pro... ${PROJECT_PATH}/${HASTE_PRO_NAME}"
-mcs -r:$UNITYENGINE_PATH,$UNITYEDITOR_PATH -target:library -define:IS_HASTE_PRO -out:$HASTE_PRO_NAME $SCRIPTS
+gmcs -r:$UNITYENGINE_PATH,$UNITYEDITOR_PATH -target:library -define:IS_HASTE_PRO -out:$HASTE_PRO_NAME $SCRIPTS
 
 echo "Done."
