db8976115e6f791e4b2280a4f0a793a84f00a28f
jimfleming <jimmy.fleming@gmail.com>
2015-02-03 14:11:15 -0800

    Adding Haste Free tips

--- Assets/Haste/Editor/HasteTips.cs
+++ Assets/Haste/Editor/HasteTips.cs
@@ -2,11 +2,18 @@ namespace Haste {
 
   public class HasteTips {
 
-    public static string[] Tips = new string[]{
-      "You can add your own commands to Haste using the MenuItem attribute.",
-      "You can use fuzzy matching to search without typing the full name.",
-      "You can access Haste's preferences in Unity's preferences window."
-    };
+    #if IS_HASTE_PRO
+      public static string[] Tips = new string[]{
+        "Add your own commands to Haste using the MenuItem attribute.",
+        "Use fuzzy matching to search without typing the full name.",
+        "Access Haste's preferences in Unity's preferences window."
+      };
+    #else
+      public static string[] Tips = new string[]{
+        "You can use fuzzy matching to search without typing the full name.",
+        "You can access Haste's preferences in Unity's preferences window."
+      };
+    #endif
 
     public static string Random {
       get {
