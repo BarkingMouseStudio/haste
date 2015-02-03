0066894cd37073dc05c829ebb0efaa0905809ff8
jimfleming <jimmy.fleming@gmail.com>
2015-02-02 18:56:09 -0800

    Adding more debug formats and cleaning up duplicate code

--- Assets/Haste/Editor/HasteDebug.cs
+++ Assets/Haste/Editor/HasteDebug.cs
@@ -7,44 +7,53 @@ namespace Haste {
 
   public static class HasteDebug {
 
-    [System.Diagnostics.Conditional("DEBUG")]
-    public static void Info(params object[] args) {
-      string[] sargs = ((IEnumerable)args)
+    static string[] Args(params object[] args) {
+      return ((IEnumerable)args)
         .Cast<object>()
         .Select(x => x == null ? "null" : x.ToString())
         .ToArray();
-      Debug.Log(String.Join(", ", sargs));
+    }
+
+    [System.Diagnostics.Conditional("DEBUG")]
+    public static void Info(params object[] args) {
+      Debug.Log(String.Join(", ", Args(args)));
     }
 
     [System.Diagnostics.Conditional("DEBUG")]
     public static void Info(string format, params object[] args) {
-      Debug.Log(String.Format(format, args));
+      Debug.Log(String.Format(format, Args(args)));
+    }
+
+    [System.Diagnostics.Conditional("DEBUG")]
+    public static void Info(string msg) {
+      Debug.Log(msg);
     }
 
     [System.Diagnostics.Conditional("DEBUG")]
     public static void Warn(params object[] args) {
-      string[] sargs = ((IEnumerable)args)
-        .Cast<object>()
-        .Select(x => x == null ? "null" : x.ToString())
-        .ToArray();
-      Debug.LogWarning(String.Join(", ", sargs));
+      Debug.LogWarning(String.Join(", ", Args(args)));
     }
 
     [System.Diagnostics.Conditional("DEBUG")]
     public static void Warn(string format, params object[] args) {
-      Debug.LogWarning(String.Format(format, args));
+      Debug.LogWarning(String.Format(format, Args(args)));
+    }
+
+    [System.Diagnostics.Conditional("DEBUG")]
+    public static void Warn(string msg) {
+      Debug.LogWarning(msg);
     }
 
     public static void Error(params object[] args) {
-      string[] sargs = ((IEnumerable)args)
-        .Cast<object>()
-        .Select(x => x == null ? "null" : x.ToString())
-        .ToArray();
-      Debug.LogError(String.Join(", ", sargs));
+      Debug.LogError(String.Join(", ", Args(args)));
     }
 
     public static void Error(string format, params object[] args) {
-      Debug.LogError(String.Format(format, args));
+      Debug.LogError(String.Format(format, Args(args)));
+    }
+
+    public static void Error(string msg) {
+      Debug.LogError(msg);
     }
   }
 }
