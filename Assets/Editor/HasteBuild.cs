8cdb3f015ce4b97bfc5b4f7822b2bd3660c12447
jimfleming <jimmy.fleming@gmail.com>
2015-02-12 18:31:40 -0800

    Adding new build script

---
+++ Assets/Editor/HasteBuild.cs
@@ -0,0 +1,16 @@
+using UnityEngine;
+using UnityEditor;
+
+namespace Haste {
+
+  public static class HasteBuild {
+
+    [MenuItem("Window/Haste/Build")]
+    public static void Build() {
+    }
+
+    [MenuItem("Window/Haste/Bundle")]
+    public static void Bundle() {
+    }
+  }
+}
