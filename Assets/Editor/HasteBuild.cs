dc8ab8f1d7b80eeb8b9ecb969ad0961905dd1c14
jimfleming <jimmy.fleming@gmail.com>
2015-02-12 20:55:10 -0800

    Adding build using Unity pipeline

--- Assets/Editor/HasteBuild.cs
+++ Assets/Editor/HasteBuild.cs
@@ -1,16 +1,76 @@
 using UnityEngine;
 using UnityEditor;
+using System;
+using System.CodeDom.Compiler;
+using System.Collections.Generic;
+using System.IO;
+using Microsoft.CSharp;
 
 namespace Haste {
 
   public static class HasteBuild {
 
-    [MenuItem("Window/Haste/Build")]
-    public static void Build() {
+    public static readonly string SOURCE_PATH = "Assets/Haste/Editor/";
+    public static readonly string INTERNAL_RESOURCES_PATH = "Assets/Haste/Editor/InternalResources/";
+
+    public static string UnityEnginePath {
+      get {
+        return Path.Combine(EditorApplication.applicationContentsPath, "Frameworks/Managed/UnityEngine.dll");
+      }
     }
 
-    [MenuItem("Window/Haste/Bundle")]
-    public static void Bundle() {
+    public static string UnityEditorPath {
+      get {
+        return Path.Combine(EditorApplication.applicationContentsPath, "Frameworks/Managed/UnityEditor.dll");
+      }
+    }
+
+    public static void LogErrors(this CompilerResults result) {
+      if (result.Errors.Count > 0) {
+        foreach (var error in result.Errors) {
+          Debug.LogError(error.ToString());
+        }
+      }
+    }
+
+    public static string[] GetSource(string buildPath) {
+      var source = new List<string>();
+
+      foreach (var file in Directory.GetFiles(buildPath, "*.cs")) {
+        source.Add(File.ReadAllText(file));
+      }
+
+      foreach (var dir in Directory.GetDirectories(buildPath)) {
+        source.AddRange(GetSource(dir));
+      }
+
+      return source.ToArray();
+    }
+
+    public static CompilerResults BuildAssembly(string[] sources, string dest, string compilerOptions) {
+      var compileParams = new CompilerParameters();
+      compileParams.OutputAssembly = dest;
+      compileParams.CompilerOptions = compilerOptions;
+      compileParams.ReferencedAssemblies.Add(UnityEnginePath);
+      compileParams.ReferencedAssemblies.Add(UnityEditorPath);
+
+      var codeProvider = new CSharpCodeProvider(new Dictionary<string, string>{ { "CompilerVersion", "v3.0" } });
+      return codeProvider.CompileAssemblyFromSource(compileParams, sources);
+    }
+
+    [MenuItem("Window/Build Haste")]
+    public static void BuildHaste() {
+      var source = GetSource(SOURCE_PATH);
+
+      var dest = EditorUtility.SaveFilePanel("Build Haste Free", "", "HasteFree.dll", "dll");
+      if (dest.Length != 0) {
+        BuildAssembly(source, dest, "").LogErrors();
+      }
+
+      dest = EditorUtility.SaveFilePanel("Build Haste Pro", "", "HastePro.dll", "dll");
+      if (dest.Length != 0) {
+        BuildAssembly(source, dest, "/optimize /define:IS_HASTE_PRO").LogErrors();
+      }
     }
   }
 }
