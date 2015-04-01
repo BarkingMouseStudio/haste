31b56b2b9bb95263830612aeccb8b56aef3a0ae2
jimfleming <jimmy.fleming@gmail.com>
2015-04-01 14:21:09 -0700

    Fixing multi-select count alignment

--- Assets/Editor/HasteBuildStyles.cs
+++ Assets/Editor/HasteBuildStyles.cs
@@ -92,7 +92,7 @@ namespace Haste {
       builder.AddStyle(new Style() { name = "Query", other = EditorStyles.textField, fixedHeight = 64, font = font, alignment = TextAnchor.MiddleLeft });
 
       builder.AddStyle(new Style() { name = "Intro", other = EditorStyles.largeLabel, fixedHeight = 64, fontSize = 32, alignment = TextAnchor.MiddleCenter });
-      builder.AddStyle(new Style() { name = "Count", other = EditorStyles.largeLabel, fontSize = 14, alignment = TextAnchor.MiddleLeft });
+      builder.AddStyle(new Style() { name = "Count", other = EditorStyles.largeLabel, fontSize = 14, alignment = TextAnchor.MiddleRight });
       builder.AddStyle(new Style() { name = "Empty", other = EditorStyles.largeLabel, fixedHeight = 24, fontSize = 16, alignment = TextAnchor.MiddleCenter });
       builder.AddStyle(new Style() { name = "Tip", other = EditorStyles.label, alignment = TextAnchor.MiddleCenter, fontSize = 14, wordWrap = true, textColor = palette.SecondaryColor });
       builder.AddStyle(new Style() { name = "Usage", other = EditorStyles.label, wordWrap = true });
