715616df461b546e24684cbad8606078878143b5
Jim Fleming <jimmy.fleming@gmail.com>
2015-06-13 01:22:27 -0700

    Removing timer from loading screen

--- Assets/Haste/Editor/GUI/HasteLoading.cs
+++ Assets/Haste/Editor/GUI/HasteLoading.cs
@@ -6,22 +6,13 @@ namespace Haste {
 
   public class HasteLoading : ScriptableObject {
 
-    public TimeSpan Duration;
-
     void OnEnable() {
       base.hideFlags = HideFlags.HideAndDontSave;
     }
 
     public void OnGUI() {
       EditorGUILayout.Space();
-
-      string label;
-      if (Duration.TotalSeconds >= 1.0) {
-        label = string.Format("Loading... {0}s", Duration.TotalSeconds);
-      } else {
-        label = string.Format("Loading... {0}ms", Duration.TotalMilliseconds);
-      }
-      EditorGUILayout.LabelField(label, HasteStyles.GetStyle("Empty"));
+      EditorGUILayout.LabelField("Loading...", HasteStyles.GetStyle("Empty"));
     }
   }
 }
