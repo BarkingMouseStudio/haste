9347425ba4a434d66a5e1fde1ef8a44f5f1acf5f
Jim Fleming <jimmy.fleming@gmail.com>
2015-06-17 12:17:13 -0700

    Adding upsell to tips and more pro tips

--- Assets/Haste/Editor/HasteTips.cs
+++ Assets/Haste/Editor/HasteTips.cs
@@ -5,22 +5,28 @@ namespace Haste {
     #if IS_HASTE_PRO
       public static string[] Tips = new string[]{
         "Add your own commands to Haste using the MenuItem attribute.",
+        "Try searching for <b>Build Settings</b> to avoid hunting it down.",
+        "Search results are ranked by usage.",
 
         "Use fuzzy matching to search without typing the full name.",
-        "Access Haste's preferences in Unity's preferences window.",
+        "Access Haste's preferences in the Unity preferences window.",
         "Try dragging an asset or object directly out of Haste.",
-        "You can select multiple results in Haste using Cmd/Ctrl+Click.",
-        "Right click on project assets, select Haste > Ignore/Unignore.",
-        "Manage ignored assets from Haste's preferences",
+        "You can select multiple results in Haste using <b>Cmd/Ctrl+Click</b>.",
+        "Select <b>Assets > Haste > Ignore/Unignore</b> to hide results.",
+        "Manage ignored assets from Haste's preferences.",
       };
     #else
       public static string[] Tips = new string[]{
+        "<b>Haste Pro</b> allows you to search Unity's menu items.",
+        "<b>Haste Pro</b> can recommend search results by usage.",
+        "<b>Haste Pro</b> bundles its source code.",
+
         "Use fuzzy matching to search without typing the full name.",
-        "Access Haste's preferences in Unity's preferences window.",
+        "Access Haste's preferences in the Unity preferences window.",
         "Try dragging an asset or object directly out of Haste.",
-        "You can select multiple results in Haste using Cmd/Ctrl+Click.",
-        "Right click on project assets, select Haste > Ignore/Unignore.",
-        "Manage ignored assets from Haste's preferences",
+        "You can select multiple results in Haste using <b>Cmd/Ctrl+Click</b>.",
+        "Select <b>Assets > Haste > Ignore/Unignore</b> to hide results.",
+        "Manage ignored assets from Haste's preferences.",
       };
     #endif
 
