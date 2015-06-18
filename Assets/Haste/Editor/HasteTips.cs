01cd836aab5a003e925d952e8e71b12d1bcdf8e0
Jim Fleming <jimmy.fleming@gmail.com>
2015-06-18 14:55:38 -0700

    Cleaning up tips

--- Assets/Haste/Editor/HasteTips.cs
+++ Assets/Haste/Editor/HasteTips.cs
@@ -6,11 +6,11 @@ namespace Haste {
       public static string[] Tips = new string[]{
         "Add your own commands to Haste using the MenuItem attribute.",
         "Try searching for <b>Build Settings</b> to avoid hunting it down.",
-        "Search results are ranked by usage.",
+        // "Search results are ranked by usage.",
 
         "Use fuzzy matching to search without typing the full name.",
         "Access Haste's preferences in the Unity preferences window.",
-        "Try dragging an asset or object directly out of Haste.",
+        "You can <b>drag and drop an Asset or GameObject</b> directly from Haste.",
         "You can select multiple results in Haste using <b>Cmd/Ctrl+Click</b>.",
         "Select <b>Assets > Haste > Ignore/Unignore</b> to hide results.",
         "Manage ignored assets from Haste's preferences.",
@@ -19,10 +19,11 @@ namespace Haste {
       public static string[] Tips = new string[]{
         "<b>Haste Pro</b> allows you to search Unity's menu items.",
         "<b>Haste Pro</b> comes bundled with its own source code.",
+        // "<b>Haste Pro</b> can recommend search results by usage.",
 
         "Use fuzzy matching to search without typing the full name.",
         "Access Haste's preferences in the Unity preferences window.",
-        "Try dragging an asset or object directly out of Haste.",
+        "You can <b>drag and drop an Asset or GameObject</b> directly from Haste.",
         "You can select multiple results in Haste using <b>Cmd/Ctrl+Click</b>.",
         "Select <b>Assets > Haste > Ignore/Unignore</b> to hide results.",
         "Manage ignored assets from Haste's preferences.",
