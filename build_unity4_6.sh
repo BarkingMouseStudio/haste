e7a0c25f09966821849d38693e7f8b57d40b88f3
Jim Fleming <jimmy.fleming@gmail.com>
2015-08-12 13:27:21 -0700

    Adding build script helpers to open Unity with the correct version

---
+++ build_unity4_6.sh
@@ -0,0 +1,2 @@
+#!/bin/sh
+env PATH=$PATH:/Applications/Unity\ 4.6.0/MonoDevelop.app/Contents/Frameworks/Mono.framework/Versions/Current/bin/ /Applications/Unity\ 4.6.0/Unity.app/Contents/MacOS/Unity
---
+++ build_unity5.sh
@@ -0,0 +1,2 @@
+#!/bin/sh
+env PATH=$PATH:/Applications/Unity\ 5.0.0/MonoDevelop.app/Contents/Frameworks/Mono.framework/Versions/Current/bin/ /Applications/Unity\ 5.0.0/Unity.app/Contents/MacOS/Unity
